a2c5510e512b5bf1fa431862142da9689e085701
fatih <fsenyigit8@gmail.com>
2024-07-21 21:20:15 +0300

    blog models

---
+++ src/models/blogModel.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+// const ModelSchema = new mongoose.Schema(
+//   {
+//     // _id: Number
+//     fieldName: {
+//       type: Number,
+//       default: null,
+//       trim: true,
+//       unique: true,
+//       index: true,
+//       // required: true,
+//       required: [true, "bunu kesin gonder"],
+//       // enum:[1,2,3],
+//       enum: [[[1, 2, 3], "bunlardan olmali"]],
+//       validate: [
+//         (data) => {
+//           return true;
+//         },
+//         "hata mesaji",
+//       ],
+//       get: (data) => data,
+//       set: (data) => data,
+//     },
+//   },
+//   {
+//     collection: "tableName",
+//     timestamps: true,
+//   },
+// );
+
+// const ModelName = mongoose.model('ModelName', ModelSchema)  
+
+const BlogCategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    }
+},{
+    collection: 'blogCategories',
+    timestamps: true
+})
+
+const BlogCategory = mongoose.model('BlogCategory', BlogCategorySchema)
+
+const BlogPostSchema = new mongoose.Schema({
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'BlogCategory',
+        required: true
+    },
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    content: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    
+},{
+    collection:'blogPosts',
+    timestamps: true
+})
+
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema)
+
+module.exports = {
+    BlogCategory: BlogCategory, 
+    BlogPost: BlogPost
+}
