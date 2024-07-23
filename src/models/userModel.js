d3ca9f6a1335fd4968d9b50f2e619a92b21cb63b
fatih <fsenyigit8@gmail.com>
2024-07-23 18:57:04 +0300

    user model

---
+++ src/models/userModel.js
@@ -0,0 +1,28 @@
+
+'use strict'
+
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+
+    email:{
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        trim: true,
+        required: true,
+    },
+    firstName: String,
+    lastName: String
+
+
+},{
+    collection :'users',
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', UserSchema)
