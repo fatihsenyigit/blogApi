dcf60f114366cc5cf66656995e336e19cd0799c0
fatih <fsenyigit8@gmail.com>
2024-07-23 17:39:16 +0300

    blog category and blog post

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -7,6 +7,15 @@ const {BlogCategory, BlogPost } = require('../models/blogModel')
 // BlogCategory controllers
 
 module.exports.blogCategory = {
+
+    list: async (req, res) => {
+        const data = await BlogCategory.find()
+        res.status(200).send({
+            error: false,
+            result: data
+        })
+    },
+
     create: async (req, res) => {
         // res.send('create method calisti')
         const data = await BlogCategory.create(req.body)
@@ -14,12 +23,85 @@ module.exports.blogCategory = {
             error: false,
             result: data
         })
+    },
+
+    read: async (req, res) => {
+        const data = await BlogCategory.findOne({_id: req.params.categoryId})
+        res.status(200).send({
+            error: false,
+            result:data
+        })
+    },
+
+    update: async (req, res)=> {
+        const data = await BlogCategory.updateOne({_id: req.params.categoryId}, req.body)
+        res.status(202).send({
+          error: false,
+          result: data,
+          new: await BlogCategory.findOne({_id: req.params.categoryId})
+        });
+    },
+    
+    delete: async (req, res) => {
+        const data = await BlogCategory.deleteOne({_id: req.params.categoryId})
+        if(data.deletedCount >=1) {
+            res.sendStatus(204)
+        } else {
+            res.errorStatusCode = 404
+            throw new Error('not found')
+        }
     }
 }
 
 
 // BlogPost controllers
 
-module.exports.blogPost = {
 
-}
+module.exports.blogPost = {
+  list: async (req, res) => {
+    const data = await BlogPost.find();
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  create: async (req, res) => {
+    const data = await BlogPost.create(req.body);
+    res.status(201).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  read: async (req, res) => {
+    const data = await BlogPost.findOne({ _id: req.params.postId });
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  update: async (req, res) => {
+    const data = await BlogPost.updateOne(
+      { _id: req.params.postId },
+      req.body,
+    );
+    res.status(202).send({
+      error: false,
+      result: data,
+      new: await BlogPost.findOne({ _id: req.params.postId }),
+    });
+  },
+
+  delete: async (req, res) => {
+    const data = await BlogPost.deleteOne({ _id: req.params.postId });
+    if (data.deletedCount >= 1) {
+      res.sendStatus(204);
+    } else {
+      res.errorStatusCode = 404;
+      throw new Error("not found");
+    }
+  },
+};
+
--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -1,13 +1,27 @@
+"use strict";
 
-'use strict';
-
-const router = require('express').Router()
+const router = require("express").Router();
 
 // call controllers
-const {blogCategory, blogPost} = require('../controllers/blogController')
+const { blogCategory, blogPost } = require("../controllers/blogController");
 
 // URL: /blog
 // BlogCategory
-router.route('/category').post(blogCategory.create)
+router.route("/category").post(blogCategory.create).get(blogCategory.list);
+router
+  .route("/category/:categoryId")
+  .get(blogCategory.read)
+  .put(blogCategory.update)
+  .patch(blogCategory.update)
+  .delete(blogCategory.delete);
 
-module.exports = router
+// BlogPost
+router.route("/post").post(blogPost.create).get(blogPost.list);
+router
+  .route("/post/:postId")
+  .get(blogPost.read)
+  .put(blogPost.update)
+  .patch(blogPost.update)
+  .delete(blogPost.delete);
+
+module.exports = router;
